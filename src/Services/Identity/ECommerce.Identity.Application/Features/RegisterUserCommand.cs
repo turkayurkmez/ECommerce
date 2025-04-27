1382954b79b36485758f7bfdf28c0d4253e7adc9
türkay ürkmez <turkay.urkmez@outlook.com>
2025-04-27 22:06:52 +0300

    Add user registration functionality and project references
    Updated `ECommerce.Identity.Application.csproj` to include references to `ECommerce.Common` and `ECommerce.Identity.Infrastructure`.
    
    Introduced `UserDto` for user data transfer and created `RegisterUserCommand` for user registration.
    
    Implemented `RegisterUserCommandHandler` to handle registration logic, including username and email uniqueness checks, password hashing, and role assignment.

---
+++ src/Services/Identity/ECommerce.Identity.Application/Features/RegisterUserCommand.cs
@@ -0,0 +1,32 @@
+﻿using ECommerce.Common.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Identity.Application.Features
+{
+    public record UserDto
+    {
+        public Guid Id { get; init; }
+        public string Username { get; init; } = string.Empty;
+        public string Email { get; init; } = string.Empty;
+        public string FirstName { get; init; } = string.Empty;
+        public string LastName { get; init; } = string.Empty;
+        public bool IsActive { get; init; }
+        public bool EmailConfirmed { get; init; }
+        public DateTime? LastLoginDate { get; init; }
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+    public class RegisterUserCommand : IRequest<Result<UserDto>>
+    {
+        public string Username { get; init; } = string.Empty;
+        public string Password { get; init; } = string.Empty;
+        public string Email { get; init; } = string.Empty;
+        public string FirstName { get; init; } = string.Empty;
+        public string LastName { get; init; } = string.Empty;
+   
+    }
+}
---
+++ src/Services/Identity/ECommerce.Identity.Application/Features/RegisterUserCommandHandler.cs
@@ -0,0 +1,65 @@
+﻿using ECommerce.Common.Results;
+using ECommerce.Identity.Domain.Entities;
+using ECommerce.Identity.Domain.Repositories;
+using ECommerce.Identity.Infrastructure.Services;
+using Mapster;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Identity.Application.Features
+{
+    public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, Result<UserDto>>
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IRoleRepository roleRepository;
+        private readonly IPasswordHashingService passwordHashingService;
+
+        public RegisterUserCommandHandler(IUserRepository userRepository, IRoleRepository roleRepository, IPasswordHashingService passwordHashingService)
+        {
+            this.userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            this.roleRepository = roleRepository ?? throw new ArgumentNullException(nameof(roleRepository));
+            this.passwordHashingService = passwordHashingService ?? throw new ArgumentNullException(nameof(passwordHashingService));
+        }
+        public async Task<Result<UserDto>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            // Kullanıcı adı eşsiz mi?
+            if (await userRepository.IsUserNameInUniqueAsync(request.Username,cancellationToken))
+            {
+                return Result<UserDto>.Failure($"Kullanıcı adı {request.Username} zaten mevcut");
+
+            }
+
+            // E-posta adresi eşsiz mi?
+            if (await userRepository.IsEmailUniqueAsync(request.Email, cancellationToken))
+            {
+                return Result<UserDto>.Failure($"E-posta adresi {request.Email} zaten mevcut");
+            }
+
+            var passwordHash = passwordHashingService.HashPassword(request.Password);
+
+            var user = new User(request.Username, request.Email, request.FirstName, request.LastName, passwordHash);
+            var defaultRoles = await roleRepository.GetDefaultRolesAsync(cancellationToken);
+            if (defaultRoles != null && defaultRoles.Count > 0)
+            {
+                foreach (var role in defaultRoles)
+                {
+                    user.AddRole(role);
+                }
+            }
+
+            await userRepository.AddAsync(user, cancellationToken);
+            var userDto = user.Adapt<UserDto>();
+            userDto.Roles = defaultRoles.Select(r => r.Name).ToList();
+
+            return Result<UserDto>.Success(userDto);
+
+
+
+        }
+    }
+    
+}
